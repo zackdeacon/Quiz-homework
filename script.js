1f3fb500b788734d6fa79e1783ecc964628dfe62
Zachary Deacon <zackdeacon347@gmail.com>
2020-06-16 20:36:36 -0700

    quiz questions diplayed

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-var startBtn = document.querySelector(".btn-start");
+var startBtn = document.getElementById("btn-start");
+var timer = document.getElementById("timer");
+var highscores = document.getElementById("highscores");
+var startingContainer = document.getElementById("starting-page");
+var quizContainer = document.getElementById("quiz-container");
+var finalContainer = document.getElementById("final-container");
+var questionTitle = document.getElementById("questiontitle")
 var secondsLeft = 75;
-var timer = document.querySelector(".timer");
-var highscores = document.querySelector(".highscores");
 var score = 0;
-var startingContainer = document.querySelector(".starting-page");
-var quizContainer = document.querySelector(".quiz-container");
-var highScoreContainer = document.querySelector("highscore-container");
-
+var currentQuestion = 0; 
 
 startBtn.addEventListener("click", function() {
    quizTimer();
     startingContainer.setAttribute("class","container d-none");
     quizContainer.setAttribute("class","container");
 })
+
+function endGame() {
+    if (secondsLeft <= 0) {
+        startingContainer.setAttribute("class","container d-none");
+        quizContainer.setAttribute("class","container d-none");
+        finalContainer.setAttribute("class","container");
+    } else {
+        console.log("still playing!")
+    }
+}
 //create start button that initiates quiz (and timer)
+function handleAnswerClick(){
+    console.log(this.textContent);
+    console.log(questions[currentQuestion].answer);
+    if(this.textContent===questions[currentQuestion].answer) {
+        currentQuestion ++;
+        generateQuestion();
+    }  else {
+            //timer interval needs to decrement by 5 for each wrong answer 
+            secondsLeft -= 5; 
+            endGame();
+            generateQuestion();
+    }
+}
+//create function to display quiz questions 
+function generateQuestion() {
+    var choicesArr = questions[currentQuestion].choices;
+    questionTitle.textContent = questions[currentQuestion].title;
+    console.log(questions[currentQuestion].title);
+    for (var i = 0; i < choicesArr.length; i++) {
+    console.log(choicesArr[i]);
+    document.getElementById("btn-" + i).textContent = choicesArr[i]; 
+    var currentBtn = document.getElementById("btn-" + i);
+    currentBtn.addEventListener("click", handleAnswerClick)
+    }
+}
 
 //create questions and answers for my quiz
 var questions = [
@@ -56,10 +92,12 @@ function quizTimer() {
   
     }, 750);
   }
-  
+  generateQuestion();
     //timer interval needs to decrement by 5 for each wrong answer 
-    
+   // secondsLeft -= 5; 
+
 //when timer hits zero or all questions have been answered the quiz is over 
+
 
 //when the game is over prompted to enter initials 
     //initials are stored with highscore 
